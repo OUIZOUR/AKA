4497f4ace602b157d0a2180670854ccbcd20f20b
Ayoub CHATTOU <ch.ayoub1@hotmail.fr>
2023-08-06 16:02:09 +0200

    spring boot intializr - backend

---
+++ backend/src/main/java/com/aka/konnach/KonnachApplication.java
@@ -0,0 +1,13 @@
+package com.aka.konnach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonnachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonnachApplication.class, args);
+	}
+
+}
