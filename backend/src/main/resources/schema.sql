d5489e8238cc04b8638af663176d059951732942
Ayoub CHATTOU <ch.ayoub1@hotmail.fr>
2023-08-13 18:37:15 +0200

    configuration H2

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE ToDo (id SERIAL PRIMARY KEY,     title VARCHAR(50),     description VARCHAR(255));
+INSERT INTO ToDo ( title, description) VALUES('Ménage','je dois faire le ménage chez moi');
